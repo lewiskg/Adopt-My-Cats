8983788fd96c90bc931a991c5eb79640e30c7f16
Kenneth Lewis <kennethlewis@Kenneths-MacBook-Pro.local>
2017-10-02 21:04:39 -0500

    setup file structure

---
+++ javascripts/domWriter.js
@@ -0,0 +1,15 @@
+"use strict";
+
+
+
+// <div class="cat-card">
+//   <div class="image-container">
+//     <img src="IMAGELINK">
+//   </div>
+//   <div class="description-container">
+//     <h3> CAT NAME </h3>
+//     <p> Color: COLORRRR </p>
+//     <p> Skills: SKILLLLLZ </p>
+//     <p class="disabled-cat"> Toes: TOESSSS </p>
+//   </div>
+// </div>
